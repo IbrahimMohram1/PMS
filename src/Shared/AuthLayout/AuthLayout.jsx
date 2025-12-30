e9b6cedc1e6fbd2b9593ecad5d0b0f93f5faaab1
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-12-30 18:59:41 +0200

    Is Update Layout

--- src/Shared/AuthLayout/AuthLayout.jsx
+++ src/Shared/AuthLayout/AuthLayout.jsx
@@ -4,7 +4,13 @@ import { Outlet } from "react-router-dom";
 export default function AuthLayout() {
   return (
     <>
-      <Outlet />
+      <div className="Auth-container h-screen">
+        <div className="forms flex justify-center items-center h-screen p-5">
+          <div className="form-layout">
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
