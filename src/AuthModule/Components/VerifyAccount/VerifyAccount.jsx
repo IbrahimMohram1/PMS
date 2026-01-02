78ec94656c1f1041f9da31723805c6ef176856ef
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2026-01-02 16:33:43 +0200

    Update : Verify Layout

--- src/AuthModule/Components/VerifyAccount/VerifyAccount.jsx
+++ src/AuthModule/Components/VerifyAccount/VerifyAccount.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useAuthApi } from "../../../Hooks/useAuth";
 import { useForm } from "react-hook-form";
 import { Button, Label, TextInput } from "flowbite-react";
-
+import VerifyBg from "../../../assets/bg3.png";
+import logo from "../../../assets/PMS.png";
 export default function VerifyAccount() {
   let { VerifyAcc, loading } = useAuthApi();
   const {
@@ -15,78 +16,88 @@ export default function VerifyAccount() {
   };
   return (
     <>
-      <div className="mx-auto  w-3/4">
-        <h6 className=" text-white  ">Welcome to PMS</h6>
-
-        <h2 className=" text-[#EF9B28] text-capitalize text-3xl mb-3 ">
-          Verify Account
-        </h2>
-        <hr className="w-8 h-1 bg-[#EF9B28]" />
-
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex   flex-col gap-4 justify-center items-center my-5"
-        >
-          <div className="  flex gap-y-3 flex-col w-full ">
-            <div>
-              <Label
-                htmlFor="email"
-                className="mb-2 text-[#EF9B28] font-medium dark:text-[#EF9B28]"
-              >
-                Email
-              </Label>
-              <TextInput
-                className="bg-transparent border-0 border-b border-gray-400 text-white placeholder-gray-300 px-0 py-2 text-lg"
-                id="email"
-                {...register("email", {
-                  required: "Email is Required",
-                  pattern: {
-                    value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
-                    message: "Please Enter a Valid Email",
-                  },
-                })}
-                type="email"
-                placeholder="Enter You mail"
-              />
-              {errors.email && (
-                <div className="bg-red-400 text-white px-3 py-2 rounded-md text-sm mt-1">
-                  {errors.email.message}
-                </div>
-              )}
+      <div
+        className="Auth-container h-screen"
+        style={{ backgroundImage: `url(${VerifyBg})` }}
+      >
+        <div className="flex  justify-center items-center h-full ">
+          <div className="w-1/2 ">
+            <div className="logo-container my-3  ">
+              <img src={logo} alt="Logo" className="mx-auto" />
             </div>
-            <div>
-              <Label
-                htmlFor="OTP"
-                className="mb-2 text-[#EF9B28] font-medium dark:text-[#EF9B28]"
-              >
-                OTP Validation
-              </Label>
-              <TextInput
-                className="bg-transparent border-0 border-b border-gray-400 text-white placeholder-gray-300 px-0 py-2 text-lg"
-                id="OTP"
-                {...register("code", {
-                  required: "OTP is Required",
-                  minLength: {
-                    value: 3,
-                    message: "Enter a Valid OTP Code",
-                  },
-                })}
-                type="password"
-                placeholder="OTP Validation"
-              />
-              {errors.code && (
-                <div className="bg-red-400 text-white px-3 py-2 rounded-md text-sm mt-1">
-                  {errors.code.message}
-                </div>
-              )}
-            </div>
-          </div>
+            <div className="form-container  p-12 rounded-2xl  bg-[#315951E5] opacity-90 ">
+              <div className="title ">
+                <p className="text-white text-sm">Welcome to PMS</p>
+                <h1 className="text-3xl text-[#EF9B28] font-bold flex flex-col">
+                  Create New Account
+                  <span className="w-4 h-1 bg-[#EF9B28]"></span>
+                </h1>
+              </div>
 
-          <Button
-            type="submit"
-            disabled={loading}
-            color="none"
-            className="
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex   flex-col gap-4 justify-center items-center my-5"
+              >
+                <div className="  flex gap-y-3 flex-col w-full ">
+                  <div>
+                    <Label
+                      htmlFor="email"
+                      className="mb-2 text-[#EF9B28] font-medium dark:text-[#EF9B28]"
+                    >
+                      Email
+                    </Label>
+                    <TextInput
+                      className="bg-transparent border-0 border-b border-gray-400 text-white placeholder-gray-300 px-0 py-2 text-lg"
+                      id="email"
+                      {...register("email", {
+                        required: "Email is Required",
+                        pattern: {
+                          value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
+                          message: "Please Enter a Valid Email",
+                        },
+                      })}
+                      type="email"
+                      placeholder="Enter You mail"
+                    />
+                    {errors.email && (
+                      <div className="bg-red-400 text-white px-3 py-2 rounded-md text-sm mt-1">
+                        {errors.email.message}
+                      </div>
+                    )}
+                  </div>
+                  <div>
+                    <Label
+                      htmlFor="OTP"
+                      className="mb-2 text-[#EF9B28] font-medium dark:text-[#EF9B28]"
+                    >
+                      OTP Validation
+                    </Label>
+                    <TextInput
+                      className="bg-transparent border-0 border-b border-gray-400 text-white placeholder-gray-300 px-0 py-2 text-lg"
+                      id="OTP"
+                      {...register("code", {
+                        required: "OTP is Required",
+                        minLength: {
+                          value: 3,
+                          message: "Enter a Valid OTP Code",
+                        },
+                      })}
+                      type="password"
+                      placeholder="OTP Validation"
+                    />
+                    {errors.code && (
+                      <div className="bg-red-400 text-white px-3 py-2 rounded-md text-sm mt-1">
+                        {errors.code.message}
+                      </div>
+                    )}
+                  </div>
+                </div>
+
+                <Button
+                  type="submit"
+                  disabled={loading}
+                  color="none"
+                  className="
             bg-[#EF9B28]
             enabled:hover:bg-[#d88a24]
             text-white
@@ -97,10 +108,13 @@ export default function VerifyAccount() {
             font-semibold
             border-none
           "
-          >
-            {loading ? "Processing..." : "Verify"}
-          </Button>
-        </form>
+                >
+                  {loading ? "Processing..." : "Verify"}
+                </Button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
