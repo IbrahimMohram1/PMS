09e5d017174d6f8b09ff711b9f067ed9b330257f
Bassant21 <bassanthossam38@gmail.com>
2026-01-02 02:58:55 +0200

    Add login form validation, update layout and assets

--- src/Shared/AuthLayout/AuthLayout.jsx
+++ src/Shared/AuthLayout/AuthLayout.jsx
@@ -4,13 +4,7 @@ import { Outlet } from "react-router-dom";
 export default function AuthLayout() {
   return (
     <>
-      <div className="Auth-container h-screen">
-        <div className="forms flex justify-center items-center h-screen p-5">
-          <div className="form-layout">
-            <Outlet />
-          </div>
-        </div>
-      </div>
+      <Outlet />
     </>
   );
 }
