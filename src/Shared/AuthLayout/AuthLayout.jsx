3e5ac428fc481be618b71101c4e01e1dfc43cf4d
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2026-01-02 16:13:36 +0200

    Update : Register Layout

--- src/Shared/AuthLayout/AuthLayout.jsx
+++ src/Shared/AuthLayout/AuthLayout.jsx
@@ -4,13 +4,7 @@ import { Outlet } from "react-router-dom";
 export default function AuthLayout() {
   return (
     <>
-      <div className="Auth-container h-screen">
-        <div className="forms flex justify-center items-center h-screen w-full ">
-          <div className="form-layout  p-5 w-1/2 mx-auto rounded-md">
-            <Outlet />
-          </div>
-        </div>
-      </div>
+      <Outlet />
     </>
   );
 }
