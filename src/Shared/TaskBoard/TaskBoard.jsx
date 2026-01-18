237b8f1c7a1a6f530687f6f367fc79307165a1d9
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2026-01-18 18:41:52 +0200

    Update : Responsive Taskboard

--- src/Shared/TaskBoard/TaskBoard.jsx
+++ src/Shared/TaskBoard/TaskBoard.jsx
@@ -126,7 +126,7 @@ export default function TaskBoard() {
 
   return (
     <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
-      <div style={{ display: "flex", gap: "20px", padding: "20px" }}>
+      <div className="flex flex-col md:flex-row gap-3 p-5">
         {columns.map((col) => (
           <Column
             key={col.id}
